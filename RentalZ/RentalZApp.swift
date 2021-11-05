a4c17ddf6eff1588acbd763dd1bb5033023a588b
phulengo <quainhanp24@gmail.com>
2021-11-06 01:42:09 +0700

    Initial Commit

---
+++ RentalZ/RentalZApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalZApp.swift
+//  RentalZ
+//
+//  Created by Phu Le Ngo on 06/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct RentalZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
